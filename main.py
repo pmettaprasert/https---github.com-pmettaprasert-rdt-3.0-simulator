1deaae710f1a08a435a80425b7085bbc958f00ce
Pete Mettaprasert <collegeforpete@gmail.com>
2023-05-07 14:32:15 -0700

    Main is done

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+from sender import Sender
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# note: no arguments will be passed in
+sender = Sender()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for i in range(1, 10):
+    # this is where your rdt_send will be called
+    sender.rdt_send('msg' + str(i))
