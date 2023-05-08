526934148b372bcb5980f6f5f2f0050052e095f6
Pete Mettaprasert <collegeforpete@gmail.com>
2023-05-07 19:33:45 -0700

    Stable version

--- receiver.py
+++ receiver.py
@@ -13,7 +13,7 @@ class Receiver:
         self.port_no = int(input("Enter the port number: "))
         self.sock = socket(AF_INET, SOCK_DGRAM)
         self.bind(self.port_no)
-        self.timeout = 20
+        self.timeout = 40
         self.sock.settimeout(self.timeout)
         self.seq_num = 0
         self.packet_num = 1
@@ -35,8 +35,8 @@ class Receiver:
     def rdt_rcv(self):
 
         print("Receiver will now be listening for packets on\nIP address: " +
-                str(gethostbyname(gethostname())) + "\nPort number: " +
-                str(self.port_no) + "\nTimeout: " + str(self.timeout) +
+              str(gethostbyname(gethostname())) + "\nPort number: " +
+              str(self.port_no) + "\nTimeout: " + str(self.timeout) +
               " seconds\n")
 
         print("Waiting for packets...\n")
@@ -51,63 +51,58 @@ class Receiver:
 
                     # if it divisible by 6, simulate timeout
                     if self.packet_num % 6 == 0:
-                        time = 4
+                        time = 6
                         print("This packet is divisible by 6. Simulating a "
                               "timeout.\nThe ack for this packet will be "
-                              "delayed by " + str(time) + " seconds.\n")
-
+                              "delayed by " + str(time) + " seconds.")
                         sleep(time)
 
-                    #if it is divisible by 3, simulate packet corruption
+                    # if it is divisible by 3, simulate packet corruption
                     elif self.packet_num % 3 == 0:
                         print("This packet is divisible by 3. Simulating a "
                               "packet corruption.\nSending back the ack for "
-                              "the previous packet.\n")
+                              "the previous packet.")
 
-                        packet = make_packet('', self.seq_num, self.seq_num)
-                        print("Packet sent: " + str(packet))
-                        print("The ACK for this packet is " + str(self.seq_num))
+                        packet = make_packet('', self.ack, self.seq_num)
 
                         self.sock.sendto(packet, addr)
 
                     # if the sequence number is the same as the expected
                     elif get_seq_num(data) == self.seq_num:
-                        print("Packet is expected, messsage: " +
+                        print("Packet is expected, messsage string delivered: " +
                               self.get_msg(data))
 
                         self.ack = self.seq_num
 
-                        print("Sending back the ack for this packet.\n")
+                        print("Sending back the ack for this packet.")
 
                         packet = make_packet('', self.ack, self.seq_num)
 
+                        self.sock.sendto(packet, addr)
                         self.seq_num ^= 1
 
+
                     else:
-                        #send ack for the previous packet
+                        # send ack for the previous packet
                         print("Incorrect sequence number. Sending back the "
-                              "ack for the previous packet.\n")
+                              "ack for the previous packet.")
                         packet = make_packet('', self.ack, self.seq_num)
                         print("Packet sent: " + str(packet))
                         self.sock.sendto(packet, addr)
 
+                print("All done for this packet.\n\n")
+                self.packet_num += 1
 
-            #need to try and see if a timeout occurs it will go into the except
+
+            #if it reaches the timeout it will break
             except:
                 break
-
 
         print("Receiver timeout has occurred. Closing the socket.\n")
         self.sock.close()
 
-
-
-
-
-
-
     def get_msg(self, data):
-        return str(data[12:])
+        return str(data[12:].decode('utf-8'))
 
 
 if __name__ == "__main__":
@@ -115,16 +110,17 @@ if __name__ == "__main__":
           " ANY COMMAND LINE ARGS.\n")
     print("The receiver class will ask for a port number so that\n"
           "it can bind to that port and wait for packets.\n")
-    print("Once the receiver class receives a packet, it will check for the\nfor"
-            "checksum. If the checksum is correct, it will check for these "
-          "scenarios:\n")
+    print(
+        "Once the receiver class receives a packet, it will check for the\nfor"
+        "checksum. If the checksum is correct, it will check for these "
+        "scenarios:\n")
     print("1. If the packet is divisible by 6, it will simulate a timeout.")
     print("2. If the packet is divisible by 3 (but divisible by 6 is checked "
-          "first), it will simulate a packet corruption.")
+          "first),\nit will simulate a packet corruption.")
     print("3. If the packet is the expected packet, it will send back the ack "
-            "for that packet.")
+          "for that packet.")
     print("4. If the packet is not the expected packet, it will send back the "
-            "ack for the previous packet.\n")
+          "ack for the previous packet.\n")
     print("The receiver class will continue to receive packets until a timeout"
-            " occurs\nwhich will close the socket automatically.\n")
+          " occurs\nwhich will close the socket automatically.\n")
     receiver = Receiver()
--- sender.py
+++ sender.py
@@ -13,7 +13,7 @@ class Sender:
         """
         print("This is the Sender class.\n")
         print("Run the receiver.py first before running this file.\nThe port "
-              "number inputted in the receiver.py will be used here to send "
+              "number inputted in the receiver.py will be used\nhere to send "
               "packet to the receiver.\n")
 
         #create a socket
@@ -25,10 +25,14 @@ class Sender:
         #ask for ip address
         self.ip_address = input("Enter the ip address of the receiver: ")
 
+        self.ip_address = self.ip_address.strip()
+
         #prompt the user for the port number
         self.port_no = int(input("Enter the port number of the receiver: "))
 
         self.seq_num = 0
+
+        self.packet_counter = 1
 
 
     def rdt_send(self, app_msg_str):
@@ -40,35 +44,69 @@ class Sender:
         """
 
         #Original message string
-        print("Original message string: " + app_msg_str)
+        print("\nOriginal message string: \"" + app_msg_str + "\"")
 
         #create a packet use the util
         packet = make_packet(app_msg_str, 0, self.seq_num)
 
         #print the packet
-        print("Packet sent: " + str(packet))
+        print("Packet sent: " + str(packet) + " with sequence number: " +
+              str(self.seq_num))
 
         #send the packet
         self.sock.sendto(packet, (self.ip_address, self.port_no))
 
         #wait for the ack, need to verify the checksum and whether is ack,
         #if not ack, resend the packet
+
+        self.received_incorrect_ack = False
+
         while True:
             try:
+
                 data, addr = self.sock.recvfrom(1024)
 
                 if not verify_checksum(data) or not get_ack_num(data) == self.seq_num:
-                    print("Packet corrupted, waiting for timeout")
+
+                    self.received_incorrect_ack = True
+                    print("Receiver acked the previous packet, waiting for "
+                          "timeout to resend packet...")
                     continue
 
             except:
-                print("Reached end of timeout, resending packet...")
+                if not self.received_incorrect_ack:
+                    print("An ACK has not been received, therefore have "
+                          "waited until timeout to resend packet...")
+                print("Reached end of timeout, resending packet with msg: " +
+                      app_msg_str + " and sequence number: " + str(
+                    self.seq_num) + "\n\n")
+
+
+                if self.received_incorrect_ack:
+                    print("[ACK-Previous retransmission]: \"" + app_msg_str + "\"")
+                else:
+                    print("[Timeout retransmission]: \"" + app_msg_str + "\"")
+
                 self.sock.sendto(packet, (self.ip_address, self.port_no))
+                self.packet_counter += 1
+                continue
 
             else:
 
-                print("Ack received, changing sequence number")
+                #print the ack and the sequence number
+                print("Packet number " + str(self.packet_counter) + " was "
+                        "received correctly.")
+                print("Ack number from receiver: " + str(get_ack_num(data)) +
+                      " == sequence number from sender: " + str(self.seq_num))
+
+                print("Changing the sequence number from " + str(
+                    self.seq_num) + " to " + str(self.seq_num ^ 1))
+
+                self.packet_counter += 1
                 self.seq_num ^= 1
+                print("All done with this packet.\n\n")
+                break
+
 
 
     ####### Your Sender class in sender.py MUST have the rdt_send(app_msg_str)  #######
